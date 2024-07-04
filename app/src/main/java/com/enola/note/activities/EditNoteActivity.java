b14a76bbb81aa26c5fb504b46d150110ed0f173f
enolalynn <enolalynn.fay@gmail.com>
2024-07-04 23:01:08 +0630

    initial

--- app/src/main/java/com/enola/note/activities/EditNoteActivity.java
+++ app/src/main/java/com/enola/note/activities/EditNoteActivity.java
@@ -1,6 +1,10 @@
 package com.enola.note.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,15 +12,72 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.enola.note.AppDatabase;
 import com.enola.note.R;
+import com.enola.note.dao.NoteDao;
+import com.enola.note.databinding.ActivityEditNoteBinding;
+import com.enola.note.entities.Note;
+
+import java.util.Date;
 
 public class EditNoteActivity extends AppCompatActivity {
-
+    private ActivityEditNoteBinding binding;
+    private Note note;
+    private NoteDao noteDao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityEditNoteBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initDatabase();
+        updateOrCreate();
+        initListener();
 
-        setContentView(R.layout.activity_edit_note);
+    }
+
+    private void initDatabase() {
+        noteDao = AppDatabase.getInstance(this).getNoteDao();
+    }
+
+    private void updateOrCreate() {
+        Intent intent = getIntent();
+        if(intent != null){
+            //update note
+            note = (Note) intent.getSerializableExtra("note");
+            if(note != null){
+                binding.etTitle.setText(note.getTitle());
+                binding.etDescription.setText(note.getDescription());
+                binding.btSaveNote.setText("Update");
+                binding.btDeleteNote.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private void initListener() {
+        binding.btDeleteNote.setOnClickListener(v -> {
+            noteDao.deleteNote(note);
+            setResult(RESULT_OK);
+            finish();
+        });
+        binding.btSaveNote.setOnClickListener(v -> {
+            String title = binding.etTitle.getText().toString();
+            String desc = binding.etDescription.getText().toString();
+            if (!title.isEmpty() && !desc.isEmpty()) {
+                if (note == null) {
+                    note = new Note(title, desc, new Date());
+                } else {
+                    note.setTitle(title);
+                    note.setDescription(desc);
+                    note.setCreated(new Date());
+                }
+                Intent intent = new Intent();
+                intent.putExtra("note", note);
+                setResult(RESULT_OK, intent);
+                finish();
+            } else {
+                Toast.makeText(this, "Fill in the blanks ...", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
--- app/src/main/java/com/enola/note/activities/MainActivity.java
+++ app/src/main/java/com/enola/note/activities/MainActivity.java
@@ -1,22 +1,104 @@
 package com.enola.note.activities;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.enola.note.AppDatabase;
 import com.enola.note.R;
+import com.enola.note.adapters.NoteAdapter;
+import com.enola.note.adapters.NoteItemClickListener;
+import com.enola.note.dao.NoteDao;
+import com.enola.note.databinding.ActivityMainBinding;
+import com.enola.note.entities.Note;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NoteItemClickListener {
+    private ActivityMainBinding binding;
+    private NoteDao noteDao;
+    private NoteAdapter noteAdapter;
+    private List<Note> notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initDatabase();
+        initListener();
 
-        setContentView(R.layout.activity_main);
+    }
 
+    private void initDatabase() {
+        noteDao = AppDatabase.getInstance(this).getNoteDao();
+        noteAdapter = new NoteAdapter();
+        noteAdapter.setOnNoteItemClickListener(this);
+        binding.rvNotes.setAdapter(noteAdapter);
+        binding.rvNotes.setLayoutManager(new LinearLayoutManager(this));
+        refreshNotes();
+    }
+
+    private void refreshNotes() {
+        notes = noteDao.getAllNotes();
+        noteAdapter.setNotes(notes);
+    }
+
+    private void initListener() {
+        binding.fabAddNote.setOnClickListener(v->{
+            Intent intent = new Intent(this, EditNoteActivity.class);
+            startActivityForResult(intent,123);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(resultCode == 123 && data != null){
+                //add new note
+
+                Note note;
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                    note = data.getSerializableExtra("note",Note.class);
+                }else{
+                    note = (Note) data.getSerializableExtra("note");
+                }
+                noteDao.addNote(note);
+                refreshNotes();
+                Toast.makeText(this, "Note saved!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == 234) {
+                //update existing note
+                if(data != null){
+                    Note note;
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                        note = data.getSerializableExtra("note", Note.class);
+                    }else {
+                        note = (Note) data.getSerializableExtra("note");
+                    }
+                    noteDao.updateNote(note);
+                    Toast.makeText(this,"Note saved!", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(this, "Delete note success!", Toast.LENGTH_SHORT).show();
+                }
+                refreshNotes();
+            }
+        }
+    }
+
+    @Override
+    public void onItemClicked(Note note) {
+        Intent intent = new Intent(this, EditNoteActivity.class);
+        intent.putExtra("note", note);
+        startActivityForResult(intent,234);
     }
 }
